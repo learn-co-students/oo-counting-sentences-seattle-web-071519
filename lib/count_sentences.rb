84d3a8cc518815266df020e9d85db21804f74d0a
ayerest <irisiseve@gmail.com>
2019-07-17 16:39:39 -0700

    finished count sentence lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
+    self.split(" ").select do |word|
+      word.sentence? || word.question? || word.exclamation?
+    end.length
 
   end
 end
