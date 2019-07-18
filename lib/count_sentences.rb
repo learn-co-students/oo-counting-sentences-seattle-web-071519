b5d2c5f3852f273407a9e07d8ac1cbaa4b7f92cc
Marisa <you@example.com>
2019-07-18 12:22:25 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+  end_with?(".")
   end
 
   def question?
-
+  end_with?("?")
   end
 
   def exclamation?
-
+  end_with?("!")
   end
 
   def count_sentences
-
+  sentences = self.split(/[.?!]/)
+var = sentences.select do |x|
+  x != ""
+ end 
+return var.count
   end
 end
+
