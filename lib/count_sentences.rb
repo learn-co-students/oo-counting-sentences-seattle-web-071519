07eeb847790003b25810448380f5dd39aa5a3076
marshallhelenm <marshallhelenm@gmail.com>
2019-07-17 17:34:04 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split(" ")
+    sentences = []
+    array.each do |chunk|
+      sentences << chunk if chunk.sentence? || chunk.question? || chunk.exclamation?
+    end
+    
+    sentences.length
   end
 end
+
+# st1 = String.new("What's up? I'm doing ok. It's cold!")
+
+# st1.count_sentences
