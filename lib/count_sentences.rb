ed40cbdf6aea2b46c320ee03a97efd1b7d6f3b5e
github username <github email address>
2019-07-20 22:36:24 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?"."
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?"?"
+      return true
+    else
+      return false
+    end
   end
+
+
+
 
   def exclamation?
+    if self.end_with?"!"
+      return true
+    else
+      return false
+    end
 
   end
-
+  # capital_count = your_string.scan(/[A-Z]/).length
   def count_sentences
+    return self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
 
   end
 end
